c8c581899961bfa922df543844d2c702b66e57a6
Willy <willy@Willys-MacBook-Pro.local>
2018-12-12 17:58:51 +0700

    create user using axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,24 +9,50 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+//Using VForm library
 import { Form, HasError, AlertError } from 'vform';
 window.Form = Form;
 Vue.component(HasError.name, HasError)
 Vue.component(AlertError.name, AlertError)
 
+//importing vue Router globally for webroute
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+//importing moment.js
+import moment from 'moment';
+
+//importing progress bar
+import VueProgressBar from 'vue-progressbar'
+//and used it 
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '3px'
+})
+
+//adding vue component
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue') },
     { path: '/profile', component: require('./components/Profile.vue') },
     { path: '/users', component: require('./components/Users.vue') }
 ]
 
+//route any path in web with history in laravel
 const router = new VueRouter({
     mode: 'history',
     routes
 })
+
+//adding filter for uppercase each word 
+Vue.filter('Uptext', function (text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+//adding filter for timestamp
+Vue.filter('myDate', function (date) {
+    return moment(date).format('MMMM Do YYYY');
+});
 
 /**
  * The following block of code may be used to automatically register your
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -22,15 +22,15 @@
                   <th>Name</th>
                   <th>Email</th>
                   <th>Type</th>
+                  <th>Register at</th>
                   <th>Modify</th>
                 </tr>
-                <tr>
-                  <td>183</td>
-                  <td>John Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span class="tag tag-success">Approved</span>
-                  </td>
+                <tr v-for="user in users" :key="user.id">
+                  <td>{{user.id}}</td>
+                  <td>{{user.name}}</td>
+                  <td>{{user.email}}</td>
+                  <td>{{user.type | Uptext}}</td>
+                  <td>{{user.created_at | myDate}}</td>
                   <td>
                     <a href>
                       <i class="fa fa-edit"></i>
@@ -63,73 +63,74 @@
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <input
-                v-model="form.name"
-                type="text"
-                name="name"
-                placeholder="Name"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('name') }"
-              >
-              <has-error :form="form" field="name"></has-error>
+          <form @submit.prevent="createUser">
+            <div class="modal-body">
+              <div class="form-group">
+                <input
+                  v-model="form.name"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('name') }"
+                >
+                <has-error :form="form" field="name"></has-error>
+              </div>
+              <div class="form-group">
+                <input
+                  v-model="form.email"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('email') }"
+                >
+                <has-error :form="form" field="email"></has-error>
+              </div>
+              <div class="form-group">
+                <textarea
+                  v-model="form.bio"
+                  name="bio"
+                  id="bio"
+                  placeholder="bio"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('bio') }"
+                ></textarea>
+                <has-error :form="form" field="bio"></has-error>
+              </div>
+              <div class="form-group">
+                <select
+                  name="type"
+                  v-model="form.type"
+                  id="type"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('type') }"
+                >
+                  <option value>Select User Role</option>
+                  <option value="admin">Admin</option>
+                  <option value="user">Standard User</option>
+                  <option value="author">Author</option>
+                </select>
+                <has-error :form="form" field="type"></has-error>
+              </div>
+              <div class="form-group">
+                <input
+                  v-model="form.password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  class="form-control"
+                  :class="{ 'is-invalid': form.errors.has('password') }"
+                >
+                <has-error :form="form" field="password"></has-error>
+              </div>
             </div>
-            <div class="form-group">
-              <input
-                v-model="form.email"
-                type="email"
-                name="email"
-                placeholder="Email"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('email') }"
-              >
-              <has-error :form="form" field="email"></has-error>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Create</button>
             </div>
-            <div class="form-group">
-              <textarea
-                v-model="form.bio"
-                name="bio"
-                id="bio"
-                placeholder="bio"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('bio') }"
-              ></textarea>
-              <has-error :form="form" field="bio"></has-error>
-            </div>
-            <div class="form-group">
-              <select
-                v-model="form.type"
-                id="type"
-                placeholder="type"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('type') }"
-              >
-                <option value>Select User Type</option>
-                <option value>Admin</option>
-                <option value>User</option>
-                <option value>Author</option>
-              </select>
-
-              <has-error :form="form" field="type"></has-error>
-            </div>
-            <div class="form-group">
-              <input
-                v-model="form.password"
-                type="password"
-                name="password"
-                id="password"
-                placeholder="password"
-                class="form-control"
-                :class="{ 'is-invalid': form.errors.has('password') }"
-              >
-              <has-error :form="form" field="password"></has-error>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Create</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
@@ -138,9 +139,12 @@
 
 <script>
 export default {
+  //Object of data User
   data() {
     return {
+      users: {},
       form: new Form({
+        id: "",
         name: "",
         email: "",
         password: "",
@@ -150,8 +154,19 @@ export default {
       })
     };
   },
-  mounted() {
-    console.log("Component mounted.");
+  methods: {
+    loadUser() {
+      axios.get("api/user").then(({ data }) => (this.users = data.data));
+    },
+    createUser() {
+      this.$Progress.start();
+      this.form.post("api/user");
+      this.$Progress.finish();
+    }
+  },
+  created() {
+    this.loadUser();
+    //console.log("Component mounted.");
   }
 };
 </script>
