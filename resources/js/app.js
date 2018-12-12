f8b1621e86392ec0a55ddd7941a499978dd44d4c
Willy <willy@Willys-MacBook-Pro.local>
2018-12-12 12:01:44 +0700

    customizing navigation color & active link in css variable

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,11 +14,14 @@ Vue.use(VueRouter)
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue') },
     { path: '/profile', component: require('./components/Profile.vue') }
+    //{ path: '/user', component: require('./components/User.vue') }
 ]
 
 const router = new VueRouter({
+    mode: 'history',
     routes
 })
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,19 +63,18 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+          <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
            <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt blue"></i>
               <p>
                 Dashboard
               </p>
-            </router-link>
+            </router-link> 
           </li>
 
           <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link ">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Management
@@ -93,19 +92,24 @@
           </li>
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user yellow"></i>
               <p>
                 Profile
               </p>
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>
-                log out
-              </p>
-            </a>
+              <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+                  <i class="nav-icon fas fa-power-off red"></i>
+                  <p>
+                  {{ __('Logout') }}
+                  </p>
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                  </form>
           </li>
           </li>
         </ul>
